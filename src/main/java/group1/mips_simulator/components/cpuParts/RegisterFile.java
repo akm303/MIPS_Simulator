cd29a3f35eeb931b5fd5cc5716f74d985d0b3bca
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-02-24 14:23:32 -0500

    lowercased all the register names

--- src/main/java/group1/mips_simulator/components/cpuParts/RegisterFile.java
+++ src/main/java/group1/mips_simulator/components/cpuParts/RegisterFile.java
@@ -8,36 +8,36 @@ public class RegisterFile {
     Design note: when hardware is taped out and manufactured, there's no way to add/remove registers.
     that's why im replacing the Vector (used in MipsComputer, which im renaming to Computer) with a preset array
      */
-    Register[] GPR = new Register[4];   //General Purpose Reg: R0-R3
-    Register[] IXR = new Register[3];   //Index Reg: X1-X3.
-    Register PC = new Register();       //Prog Counter Reg : address of next instruction to be executed
-    Register CC = new Register();       //Condition Code Reg : set when arith/logical operations executed
+    Register[] gpr = new Register[4];   //General Purpose Reg: R0-R3
+    Register[] ixr = new Register[3];   //Index Reg: X1-X3.
+    Register pc = new Register();       //Prog Counter Reg : address of next instruction to be executed
+    Register cc = new Register();       //Condition Code Reg : set when arith/logical operations executed
                                         // ONZE (Overflow, uNderflow, divZero, Equalornot)
                                 // (only one i dont understand is "equal or not", is that saying, set flat to 1 if equal
                                 // and set to 0 if not?)
-    Register IR = new Register();       //Instruction Reg : holds instruction to be exe
-    Register MAR = new Register();      //Mem Addr Reg : holds address of word to be fetched from memory
-    Register MBR = new Register();      //Mem Buffer Reg : holds word just-fetched-from/to-be-writen-to memory
-    Register MFR = new Register();      //Mem Fault Reg : contains ID code if machine fault after it occurs
-    //Register[] FPR = new Register[2]; //float point register placeholder <<DO NOT IMPLEMENT TILL PART 4>>
+    Register ir = new Register();       //Instruction Reg : holds instruction to be exe
+    Register mar = new Register();      //Mem Addr Reg : holds address of word to be fetched from memory
+    Register mbr = new Register();      //Mem Buffer Reg : holds word just-fetched-from/to-be-writen-to memory
+    Register mfr = new Register();      //Mem Fault Reg : contains ID code if machine fault after it occurs
+    //Register[] fpr = new Register[2]; //float point register placeholder <<DO NOT IMPLEMENT TILL PART 4>>
     //Other registers add here as we need them (prof said he hasnt given us all of them)
 
     public RegisterFile(){
         //initialize all reg w/ null
         Value nullValue = new Value(0);
 
-        for (Register reg : GPR){
+        for (Register reg : gpr){
             reg.set(nullValue);
         }
-        for (Register reg : IXR){
+        for (Register reg : ixr){
             reg.set(nullValue);
         }
         //for (Register reg : FPR){ // <<DO NOT IMPLEMENT TILL PART 4>>
         //    reg.write(nullValue);
         //}
 
-        Register[] allOtherReg = new Register[]{PC,CC,IR,MAR,MBR,MFR};
-        //Note* add FPR and other registers as you update Reg File implementation
+        Register[] allOtherReg = new Register[]{pc,cc,ir,mar,mbr,mfr};
+        //Note* add fpr and other registers as you update Reg File implementation
         for (Register reg : allOtherReg) {
             reg.set(nullValue);
         }
@@ -49,7 +49,7 @@ public class RegisterFile {
         * i.e. to access R1, call ~.getGPR(1)
         * */
         assert((i >= 0) && (i <= 3));
-        return GPR[i];
+        return gpr[i];
     }
 
     public Register getIXR(int i){
@@ -57,26 +57,26 @@ public class RegisterFile {
          * i.e. to access I1, call ~.getGPR(1)
          * */
         assert((i >= 1) && (i <=3));
-        return IXR[i-1];
+        return ixr[i-1];
     }
 
     public Register getPC(){
-        return this.PC;
+        return this.pc;
     }
     public Register getCC(){
-        return this.CC;
+        return this.cc;
     }
     public Register getIR(){
-        return this.IR;
+        return this.ir;
     }
     public Register getMAR(){
-        return this.MAR;
+        return this.mar;
     }
     public Register getMBR(){
-        return this.MBR;
+        return this.mbr;
     }
     public Register getMFR(){
-        return this.MFR;
+        return this.mfr;
     }
 //    public Register getFPR() { return this.FPR; }
 
