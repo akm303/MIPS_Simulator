5b6d02c057ed3b8cb1edc6d6be7547d3e2e81524
Arthur Bacon <bacon247@gmail.com>
2024-02-24 09:16:19 -0500

    Initial work on building out the front end

--- src/main/java/group1/mips_simulator/SimApplication.java
+++ src/main/java/group1/mips_simulator/SimApplication.java
@@ -3,18 +3,51 @@ package group1.mips_simulator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SimApplication extends Application {
+
+    static final int WIDTH = 900;
+    static final int HEIGHT = 750;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SimApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+
+//        HBox hBox_fullRegDisplay = new HBox();
+//        VBox vBox_leftColumnRegs = new VBox();
+//        vBox_leftColumnRegs.getChildren().add(makeRegisterAndLabel("left", "0000000000000000"));
+//
+//        VBox vBox_middleColumnRegs = new VBox();
+//        vBox_middleColumnRegs.getChildren().add(makeRegisterAndLabel("Middle", "0000000000000000"));
+//
+//        VBox vBox_rightColumnRegs = new VBox();
+//        vBox_rightColumnRegs.getChildren().add(makeRegisterAndLabel("PC", "000000000000"));
+//
+//        hBox_fullRegDisplay.getChildren().addAll(vBox_leftColumnRegs, vBox_middleColumnRegs, vBox_rightColumnRegs);
+
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public HBox makeRegisterAndLabel(String registerName, String initialValue) {
+        Label label1 = new Label(registerName + ":");
+        TextField textField = new TextField(initialValue);
+
+        HBox hb = new HBox();
+        hb.getChildren().addAll(label1, textField);
+        hb.setSpacing(10);
+
+        return hb;
     }
 
     public static void main(String[] args) {
--- src/main/java/group1/mips_simulator/SimController.java
+++ src/main/java/group1/mips_simulator/SimController.java
@@ -2,10 +2,34 @@ package group1.mips_simulator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class SimController {
+    public TextField GPR1;
+    public TextField GPR2;
+    public TextField GPR3;
+    @FXML
+    public TextField GPR0;
+    public TextField IXR3;
+    public TextField IXR2;
+    public TextField IXR1;
+    public VBox VBoxLeft_GPRs;
+    public VBox VBoxCenter_IXRs;
+    public VBox VBoxRight_OtherRegs;
+    public TextField PC;
+    public TextField MAR;
+    public TextField MBR;
+    public TextField RI;
+    public TextField MFR;
+    public TextField Privileged;
+    public TextField CC;
+
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Label pcLabel;
 
     @FXML
     protected void onHelloButtonClick() {
