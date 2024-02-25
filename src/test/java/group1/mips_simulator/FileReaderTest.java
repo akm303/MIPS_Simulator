9c05fdaeb26aa869dbd4ff1aee6e50d251b67253
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-02-25 16:52:49 -0500

    addition of try/catch for FileReaderTest, since it threw error during build

--- src/test/java/group1/mips_simulator/FileReaderTest.java
+++ src/test/java/group1/mips_simulator/FileReaderTest.java
@@ -4,6 +4,7 @@ import group1.mips_simulator.components.instructionParts.Instruction;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +14,12 @@ class FileReaderTest {
     @Test
     void readBinaryFile() {
         FileReader underTest = new FileReader();
-        Vector<Instruction> result = underTest.readBinaryFile("src/test/java/group1/mips_simulator/testFile.txt");
+        Vector<Instruction> result = null;
+        try {
+            result = underTest.readBinaryFile("src/test/java/group1/mips_simulator/testFile.txt");
+        } catch (IOException e) {
+            throw new RuntimeException("Unexpected runtime error in FileReaderTest.readBinaryFile()");
+        }
         assertEquals(result.size(), 41);
     }
 }
