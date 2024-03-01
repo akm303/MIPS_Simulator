1d0d07ef396215d443f44f349f08bc2c4e68dbb1
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-03-01 00:29:36 -0500

    added instruction unit test for short conversion

--- src/test/java/group1/mips_simulator/components/instruction/InstructionTest.java
+++ src/test/java/group1/mips_simulator/components/instruction/InstructionTest.java
@@ -17,4 +17,13 @@ class InstructionTest {
         Instruction ldr = factory.buildInstruction_fromBinary("0000010101101101");
         assertEquals("0000010101101101", ldr.toString_Binary());
     }
+
+    @Test
+    void toShort(){
+        InstructionFactory factory = new InstructionFactory();
+        Instruction hlt = factory.buildInstruction_fromBinary("0000000000000000");
+        assertEquals((short) 0, hlt.toShort());
+
+        Instruction ldr = factory.buildInstruction_fromBinary("0000010101101101");
+        assertEquals((short) 0b0000010101101101, ldr.toShort());    }
 }
