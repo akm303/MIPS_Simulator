4f2a2389099df59a74317f92996c0a3d83ff1abd
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-03-21 12:11:44 -0400

    added get and set methods

--- src/main/java/group1/mips_simulator/components/floatParts/Float.java
+++ src/main/java/group1/mips_simulator/components/floatParts/Float.java
@@ -4,4 +4,12 @@ import group1.mips_simulator.components.Value;
 
 public class Float {
     protected Value value = new Value(0);
+
+    public Value get() {
+        return this.value;
+    }
+
+    public void set(Value newValue) {
+        value = newValue;
+    }
 }
