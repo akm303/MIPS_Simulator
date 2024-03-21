9c659f57db19a6ad2523243ac7d85741485d389a
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-03-21 19:22:59 -0400

    added accesscounter for fullyl associative cache

--- src/main/java/group1/mips_simulator/components/memParts/Cache.java
+++ src/main/java/group1/mips_simulator/components/memParts/Cache.java
@@ -8,9 +8,17 @@ public class Cache extends Memory{
     fully associative
      */
     CacheLine[] blocks = new CacheLine[Config.CACHE_LINES];
+    short[] lastAccessedArray = new short[Config.CACHE_LINES];
+    // for fully-associative cache, each line has a counter to let you know
+    // when it was last accessed (to decide which line to overwrite when needed)
+    // every time a memory access occurs (load/store), all non-accessed memory
+    // increments by 1
+    // the last memory to be accessed (either written in, or read from) has their
+    // count set to 0
 
     public Cache(){
-
+        for(int i = 0; i < lastAccessedArray.length; i++)
+            lastAccessedArray[i] = 0; //set all counts to -
     }
 
     public CacheLine getLine(int lineNumber){
@@ -20,5 +28,12 @@ public class Cache extends Memory{
     public void setLine(int lineNumber, Memory memory){
         CacheLine line = blocks[lineNumber];
 
+    }
+
+    public void updateAccessCounts(int lineNumber){
+        for(int i = 0; i < lastAccessedArray.length; i++){
+            lastAccessedArray[i] += 1; //update all
+        }
+        lastAccessedArray[lineNumber] = 0; //then set current line count to 0
     }
 }
