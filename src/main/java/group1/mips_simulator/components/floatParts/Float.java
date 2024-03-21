f8d8c2d24a5bcbd4b5001545ae3120215f72c225
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-03-21 12:24:34 -0400

    comments on how to calculate bias, and float point values

--- src/main/java/group1/mips_simulator/components/floatParts/Float.java
+++ src/main/java/group1/mips_simulator/components/floatParts/Float.java
@@ -4,6 +4,24 @@ import group1.mips_simulator.components.Value;
 
 public class Float {
     protected Value value = new Value(0);
+    //in general, float data has the following fields, each with a purpose in calculating the float
+    boolean signBit; //MSB (most significant bit, always indicates sign. Floats dont have an unsigned form)
+    short bias; // the bits in the second field are converted to a unsigned int, then subtracted from the exponent bias
+                //  (b = 2^(e-1) - 1) //bias is b, exponent has e bits; this is how you calculate bias
+                //
+
+    public short read(){
+        /* read value of this float as a short
+        Note that this is a representation of the float,
+        and not of the data directly stored in a Float
+        */
+        short rvalue = 0;
+        short fdata = value.getShort();
+
+
+
+        return rvalue;
+    }
 
     public Value get() {
         return this.value;
@@ -12,4 +30,8 @@ public class Float {
     public void set(Value newValue) {
         value = newValue;
     }
+
+
+
+
 }
