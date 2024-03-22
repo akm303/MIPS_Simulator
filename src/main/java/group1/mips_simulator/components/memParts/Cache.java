aa7a0b70bf1c400fadcb15db0417e97e6c3a839f
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-03-22 19:14:09 -0400

    refactored teh whole cache, now its ADT based

--- src/main/java/group1/mips_simulator/components/memParts/Cache.java
+++ src/main/java/group1/mips_simulator/components/memParts/Cache.java
@@ -2,64 +2,79 @@ package group1.mips_simulator.components.memParts;
 
 import  group1.mips_simulator.components.Config;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Cache extends Memory{
     /*
-    16 line
-    fully associative
+    16 line, fully associative cache
+    Write Through (writes to cache, and immediately writes to memory (shouldnt need to implement separately since
+    in Java, variables names should be references to the same objects))
+
     Cache is accessed usually by seeing if a certain data location is in a cache
-    i.e. if the binary of a memory location has a tag that matches a line tag in cache.
+    (i.e. if the Word of a memory location has a tag that we extract, which should a line tag in cache.)
+
+    Fully associative cache means we can write to any line in cache, and we do so by writing over the oldest line first
+    (i.e. FIFO)
      */
-    CacheLine[] blocks = new CacheLine[Config.CACHE_LINES];
-    short[] lastAccessedArray = new short[Config.CACHE_LINES];
-    // for fully-associative cache, each line has a counter to let you know
-    // when it was last accessed (to decide which line to overwrite when needed)
-    // every time a memory access occurs (load/store), all non-accessed memory
-    // increments by 1
-    // the last memory to be accessed (either written in, or read from) has their
-    // count set to 0
+    Memory memory; //reference to memory that cache is linked to
+    int cacheSize = Config.CACHE_LINES;
 
-    public Cache(){
-        //set all counts to 0
-        Arrays.fill(lastAccessedArray, (short) 0);
+    //data structures
+    Queue<Short> cacheQueue; //going to use list as a queue for now
+    Map<Short,CacheLine> cacheLines; //
+
+    //    CacheLine[] lines = new CacheLine[cacheSize];
+
+    public Cache(Memory _memory){
+        //cache has a memory it references
+        memory = _memory;
     }
 
-    public CacheLine getLine(int dataAddress){
+    public CacheLine getLineFromCache(short dataAddr){
         // get the full line of data at that line number
-        int lineNumber = getLineTag(dataAddress);
-        updateAccessCounts(lineNumber);
-        return blocks[lineNumber];
+        CacheLine returnLine;
+
+        short tag = getLineTag(dataAddr);
+
+        if(cacheLines.containsKey(tag))
+            returnLine = cacheLines.get(tag);
+        else{
+            if(cacheQueue.size() < cacheSize) { //if space in the cache
+                cacheQueue.add(tag);
+                cacheLines.put(tag, new CacheLine(dataAddr,memory));
+            }
+            else{ //else no more space, must replace a line
+                Short tagToRemove = cacheQueue.poll(); //remove top CacheLine from Cache
+                cacheLines.remove(tagToRemove); //remove that tag from the lines Map
+                cacheQueue.add(tag); //add the new tag to the cache
+                cacheLines.put(tag, new CacheLine(dataAddr, memory));
+            }
+            returnLine = cacheLines.get(tag);
+        }
+        return returnLine;
     }
 
-    public void setLine(int dataAddress,Memory memory){
-        // todo: set the line with highest access count with the data from memory at those locations.
-        // make sure correct items in memory are getting pulled,
 
-        int lineNumber = getLineTag(dataAddress);
-        CacheLine line = blocks[lineNumber];
-        for(int i = 0; i < Config.ENTRIES_PER_BLOCK; i++)
-            line.setEntry(i,memory.get(dataAddress));
-        updateAccessCounts(lineNumber);
-    }
+//        CacheLine tagLine = new CacheLine(tag,memory); //get cache line with that tag from memory
+//        // if tag is in cache
+//        for(int i = 0; i < cacheQueue.size(); i++){
+//
+//        }
 
-    public void updateAccessCounts(int lineNumber){
-        for(int i = 0; i < lastAccessedArray.length; i++)
-            lastAccessedArray[i] += 1;      //update all counters (will reset appropriate one to 0 at end)
-        lastAccessedArray[lineNumber] = 0;  //then set current line count to 0
-    }
+//    }
 
-    private int getLineTag(int dataAddress){
+
+//    public CacheLine getLineFromMemory(short dataAddr, Memory memory){
+//        short tag = getLineTag(dataAddr);
+//        cacheQueue.add(tag);
+//        return new CacheLine(dataAddr,memory);
+//    }
+
+    private short getLineTag(short dataAddress){
         //todo: get tag bits of memory location references.
         // return an integer of it
-        int tag = 0;
-        return tag;
+        short lineTag = (short)((int)dataAddress % 0b1111);
+        return lineTag;
     }
 
-    private boolean inCache(int dataAddress) {
-        int tag = getLineTag(dataAddress);
-        for(int i = 0; i < lastAccessedArray.length; i++){
-
-        }
-    }
 }
--- src/main/java/group1/mips_simulator/components/memParts/CacheLine.java
+++ src/main/java/group1/mips_simulator/components/memParts/CacheLine.java
@@ -1,39 +1,52 @@
 package group1.mips_simulator.components.memParts;
 
 import group1.mips_simulator.components.Word;
-import group1.mips_simulator.components.memParts.Storage;
 import group1.mips_simulator.components.Config;
+import group1.mips_simulator.components.memParts.Storage;
 
-import java.util.Arrays;
 
 public class CacheLine{
     // one line has 8 words
     // tag is    vvv  for
     // octal [88 888 8]
 
+    int blockSize = Config.ENTRIES_PER_BLOCK;
     public Word[] entries; //8 words per block, each word is an entry
 
-    public CacheLine(){
-        entries = new Word[Config.ENTRIES_PER_BLOCK];
-        Arrays.fill(entries,new Word(0));
+    public CacheLine(int memAddr, Memory memory){
+        int startOfBlock = memAddr - (memAddr % blockSize);
+        entries = new Word[blockSize];
+
+        for(int i = 0; i < entries.length; i++){
+            // set entry at block index i to be
+            // memory's entry at address i
+            setEntry(i, memory.get(startOfBlock + i));
+        }
     }
 
     public Word getEntry(int addressLocation){
         //todo: write unit tests
-        int blockNumber = getBlockNumber(addressLocation);
-        return entries[blockNumber];
+        int blockIdx = getIndex(addressLocation);
+        return entries[blockIdx];
     }
 
     public void setEntry(int addressLocation, Word entry){
         //todo: write unit tests
-        int blockNumber = getBlockNumber(addressLocation);
-        entries[blockNumber] = entry;
+        // if we are doing write through, i need to know if
+        // java considers entries[blockIdx] and memory[addressLocation] to be references
+        // to the same block of memory
+        // otherwise need to do a second write directly to memory, within this function
+        int blockIdx = getIndex(addressLocation);
+        entries[blockIdx] = entry;
     }
 
-    public int getBlockNumber(int addressLocation){
+
+    public int getIndex(int addressLocation){
         //todo: write unit tests
+        // also pretty sure its not right, but need to check spec
         return (addressLocation | 0b111);
     }
+
 
 
 }
