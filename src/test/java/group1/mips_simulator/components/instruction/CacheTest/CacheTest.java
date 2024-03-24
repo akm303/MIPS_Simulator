aaba67ec101031c07a713107fd16bd8b0c26f605
Ari Majumdar <akm303@gwmail.gwu.edu>
2024-03-23 21:22:34 -0400

    adjusting the tests, need a way to figure out how to print cache lines

--- src/test/java/group1/mips_simulator/components/instruction/CacheTest/CacheTest.java
+++ src/test/java/group1/mips_simulator/components/instruction/CacheTest/CacheTest.java
@@ -12,23 +12,22 @@ public class CacheTest {
     int TEST_MEM_SIZE = 16;
     Memory mem = new Memory(TEST_MEM_SIZE);
 
-    Cache c = new Cache(mem);
+    Cache c;
     CacheLine l = new CacheLine(3,mem);
 
     private void setupTest(){
         short[] valuesToWrite = new short[]{
-                0,
-                (short)0xFFFF,
-                (short)0x1234,
-                (short)0xBEEF,
-                (short)0xA8A8,
-                9827,
-                (short)0b1100_1001_0010_0010
+                0,   (short)0xFFFF,(short)0x1234,(short)0xBEEF,
+                0,   (short)0xA8A8,    0,  0,
+                9827,(short)0xC922, 0,(short)0x7FFF,
+                0,  0,  8,  0
         };
 
         for(int i=0;i<TEST_MEM_SIZE;i++){
             mem.write((short) i, valuesToWrite[i]);
         }
+
+        c = new Cache(mem);
     }
 
     @Test
@@ -42,6 +41,14 @@ public class CacheTest {
             // System.out.println(" Tag:" + Utility.shortToBinaryString(tag,16));
             assertEquals((short)((address & 0xFFF8)>>3),tag);
         }
+
+
+    }
+
+    @Test
+    void Cache(){
+        setupTest();
+        c.getLineFromCache((short) 0x1234);
 
         for(int i = 0; i < TEST_MEM_SIZE; i++){
             short data = mem.read(i);
