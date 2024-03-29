b6f2638f89b637bc506c9b88e0217019325a7a14
Arthur Bacon <bacon247@gmail.com>
2024-03-29 16:30:43 -0400

    Nocab fix unit tests

--- src/main/java/group1/mips_simulator/components/instructionParts/instruction/Instruction.java
+++ src/main/java/group1/mips_simulator/components/instructionParts/instruction/Instruction.java
@@ -1,5 +1,6 @@
 package group1.mips_simulator.components.instructionParts.instruction;
 
+import group1.mips_simulator.Utility;
 import group1.mips_simulator.components.Word;
 import group1.mips_simulator.components.instructionParts.Field;
 import group1.mips_simulator.components.instructionParts.OpCode;
@@ -25,20 +26,14 @@ public class Instruction {
             result.append(f.toBinString());
             result.append(" ");
         }
-        return result.toString();
+        return result.toString().trim();
     }
 
 
     public short toShort() {
         // generates an int from string of self,
         // returns a short casting of this value
-        int rvalue = 0;
-        char[] instructions = toString_Binary().toCharArray();
-        for (int i = 0; i < instructions.length; i++) {
-            if (instructions[instructions.length - i - 1] == '1')
-                rvalue |= 1 << i;
-        }
-        return (short) rvalue;
+        return Utility.binaryToShort(toString_Binary());
     }
 
     public Word toWord() {
